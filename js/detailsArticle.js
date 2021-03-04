5cb15d80772b93fc3041dbffeecb4a4efd129567
Jan <jan.worbsto@gmail.com>
2021-03-04 13:47:45 +0100

    edit section added

--- js/detailsArticle.js
+++ js/detailsArticle.js
@@ -4,18 +4,18 @@ const container = document.querySelector('.container');
 const deleteBtn = document.querySelector('.delete-btn');
 
 
+
 const artDetails = async () => {
     
     const res = await fetch('http://localhost:3000/articles/'+id);
     const article = await res.json();
     
-
+    deleteBtn.style.display= 'block';
+    
     const template2 = `
     <img src="${article.img}">
     <h1>${article.title}</h1><br>
     <p>${article.body}</p>
-    
-    
 
     `;
     container.innerHTML = template2;
@@ -24,15 +24,31 @@ const artDetails = async () => {
 
 
 
-deleteBtn.addEventListener('click', function deleteFuction (el) {
+const deleteFuction = async (el) => {
+
     
-    const res = fetch('http://localhost:3000/articles/'+id, {
+
+    const res = await fetch('http://localhost:3000/articles/'+id, {
         method: 'DELETE'
     });
-
-    window.location.replace('index.html');
     
-});
+    window.location.replace('index.html');
+};
 
+
+const alertFunction = () => {
+
+    if (confirm('Czy jestesś pewien!?')) {
+        deleteFuction();
+    } else {
+        false
+    };
+};
+
+
+
+
+
+deleteBtn.addEventListener('click', alertFunction);
 
 window.addEventListener('DOMContentLoaded', () => artDetails ());
--- js/scriptA.js
+++ js/scriptA.js
@@ -1,10 +1,5 @@
 const menuSections = document.querySelectorAll('.menu-section');
 const menuTabs = document.querySelectorAll('.menu-tab');
-const btntest = document.querySelector('.btntest')
-
-
-
-
 
 
 const showInfo = id => {
@@ -18,32 +13,26 @@ const showInfo = id => {
         tab.classList.remove('menu-tab-active');
     });
 
-    
     document.getElementById(id).style.display = 'block';
     // clasDelate.style.width = '82vw';
-    
 
     const currentActiveButton = document.querySelector(`[data-id=${id}]`);
     currentActiveButton.classList.add('menu-tab-active');
     
 };
 
-// GET POSTS FROM JSON
 
+// GET POSTS FROM JSON
 
 const contentArticles = document.querySelector('.content-article');
 
 const getPosts = async () => {
 
-    
-
     let url = 'http://localhost:3000/articles';
-    
-    
+ 
     const res = await fetch(url);
     const articles = await res.json();
-    
-
+  
     let template = ''; //dopytac o to czemu pusty string??
     articles.forEach(el => {
         template += `
@@ -54,12 +43,9 @@ const getPosts = async () => {
             <a href ="moredetails.html?id=${el.id}">read more...</a>
         </div>
         `
-        
-        
     });
 
     contentArticles.innerHTML = template;
-
 }
 
 window.addEventListener('DOMContentLoaded', () => getPosts ());
