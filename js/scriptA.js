662510f36ff9a4b5ac7e70a8d8047cc4f7073c65
Jan <jan.worbsto@gmail.com>
2021-03-11 21:59:19 +0100

    buttons to toggling pages positioned and added restriction

--- js/scriptA.js
+++ js/scriptA.js
@@ -3,9 +3,10 @@ const menuTabs = document.querySelectorAll('.menu-tab');
 const searchInput = document.querySelector('#search');
 const incBtn = document.querySelector('.inc-btn');
 const decBtn = document.querySelector('.dec-btn');
-
+const pageNr = document.querySelector('.page-nr');
 let incPage = 1;
 
+// TABS TOGLER
 
 const showInfo = id => {
     
@@ -26,21 +27,28 @@ const showInfo = id => {
     
 };
 
-
 // GET POSTS FROM JSON
 
 const contentArticles = document.querySelector('.content-article');
 
 const getPosts = async (term) => {
 
-    let url = `http://localhost:3000/articles?_sort=id&_order=desc&_page=${incPage}&_limit=5`;
-
+    let url = `http://localhost:3000/articles?_sort=id&_order=desc&_page=${incPage}&_limit=7`;
+     console.log(url)
     if(term) {
         url += `&q=${term}`;
     };
- 
+    
+    pageNr.innerText = `${incPage}`
+
     const res = await fetch(url);
     const articles = await res.json();
+
+    if (Object.keys(articles).length  === 2 || Object.keys(articles).length <= 1) {
+        incBtn.disabled = true;
+    } else if (Object.keys(articles).length > 0 || Object.keys(articles).length >= 1) {
+        incBtn.disabled = false;
+    };
   
     let template = ''; //dopytac o to czemu pusty string??
     articles.forEach(el => {
@@ -53,39 +61,38 @@ const getPosts = async (term) => {
         </div>
         `
     });
-
     contentArticles.innerHTML = template;
 };
 
+// PAGE COUNTER
+
 const paginatedInc = () => {
     incPage++;
-
-    console.log(incPage);
-    getPosts()
-    
+    getPosts()  
 };
 
 const paginatedDec = () => {
-    incPage--;
+    if (incPage < 1) {
+        decBtn.disabled = true;
+
+    } else if (incPage > 1){  
+        decBtn.disabled = false;
+        incPage--;
+    };
 
     console.log(incPage);
     getPosts()
-    
 };
- 
+
 incBtn.addEventListener('click', paginatedInc);
 decBtn.addEventListener('click', paginatedDec);
 
-
-
-
-
+// SEARCH INPUT 
 searchInput.addEventListener('keyup', searchFunction = (e) => {
 
     if(e.keyCode === 13) {
-        getPosts(searchInput.value.trim())
+        getPosts(searchInput.value.trim());     
     };
-    
 });
 
 window.addEventListener('DOMContentLoaded', () => getPosts ());
